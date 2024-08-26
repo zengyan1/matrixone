6073a7fa183548ef936387f5c88634fb17766417
zengyan1 <171838137@qq.com>
2024-08-26 21:23:44 +0800

    cachesize

--- pkg/objectio/cache.go
+++ pkg/objectio/cache.go
@@ -93,7 +93,7 @@ func metaCacheSize() int64 {
 	if total < 32*mpool.GB {
 		return 1 * mpool.GB
 	}
-	return 4 * mpool.GB
+	return 8 * mpool.GB
 }
 
 func shardMetaCacheKey(key mataCacheKey) uint8 {
